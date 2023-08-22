470c4b469314ef1d04fe86e9a8ef35beab2be199
Halil <halilattar01@gmail.com>
2023-08-22 14:50:02 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,16 +46,15 @@ dependencies {
 
 
    val lifecycle_version = "2.6.1"
-   annotationProcessor( "com.google.dagger:hilt-compiler:2.47")
+   annotationProcessor("com.google.dagger:hilt-compiler:2.47")
    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
 
 //Hilt
    implementation("com.google.dagger:hilt-android:2.47")
-  kapt("com.google.dagger:hilt-android-compiler:2.47")
+   kapt("com.google.dagger:hilt-android-compiler:2.47")
    //navigation
    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-
 
 
    val room_version = "2.5.2"
@@ -63,26 +62,26 @@ dependencies {
    implementation("androidx.room:room-runtime:$room_version")
    annotationProcessor("androidx.room:room-compiler:$room_version")
 
-   // To use Kotlin annotation processing tool (kapt)
+
    kapt("androidx.room:room-compiler:$room_version")
-   implementation ("androidx.room:room-ktx:2.5.2")
+   implementation("androidx.room:room-ktx:2.5.2")
    // retrofit
 
    implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
 // GSON
 
-   implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 // coroutine
 
-   implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
+   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
 
-   implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
 
    //Glide
 
-   implementation ("com.github.bumptech.glide:glide:4.15.1")
+   implementation("com.github.bumptech.glide:glide:4.15.1")
 
    implementation("androidx.core:core-ktx:1.10.1")
    implementation("androidx.appcompat:appcompat:1.6.1")
@@ -92,13 +91,13 @@ dependencies {
    androidTestImplementation("androidx.test.ext:junit:1.1.5")
    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-   implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-   implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-   implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
+   implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+   implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
 
-   implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-   implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-   implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
+   implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+   implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
 
 }
 kapt {
